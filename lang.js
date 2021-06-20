b581ef650b115bafe6959be2a8c37df824ceae63
Vladlen Bykov <bykovvvladlen@gmail.com>
2021-06-20 16:23:15 +0300

    add files

---
+++ lang.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'save': {
+    ru: 'Сохранить',
+    en: 'Save',
+  },
+  'delete': {
+    ru: 'Удалить',
+    en: 'Delete',
+  },
+  'edit': {
+    ru: 'Изменить',
+    en: 'Edit',
+  },
+};
